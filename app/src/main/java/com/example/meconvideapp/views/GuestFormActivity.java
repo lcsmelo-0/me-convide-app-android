18e11a29f6f768a61a50c433d161ecb9f09b7033
Lucas de Melo Lima <luucasmell@gmail.com>
2020-05-24 20:53:39 -0300

    Configure the database entity

--- app/src/main/java/com/example/meconvideapp/views/GuestFormActivity.java
+++ app/src/main/java/com/example/meconvideapp/views/GuestFormActivity.java
@@ -3,14 +3,62 @@ package com.example.meconvideapp.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.meconvideapp.R;
+import com.example.meconvideapp.constants.GuestConstants;
+import com.example.meconvideapp.entities.GuestEntity;
 
-public class GuestFormActivity extends AppCompatActivity {
+public class GuestFormActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_form);
+
+        this.mViewHolder.mEditName = this.findViewById(R.id.edit_name);
+        this.mViewHolder.mRadioNotConfirmed = this.findViewById(R.id.radio_not_confirmed);
+        this.mViewHolder.mRadioPresent = this.findViewById(R.id.radio_present);
+        this.mViewHolder.mRadioAbsent = this.findViewById(R.id.radio_absent);
+        this.mViewHolder.mButtonSave = this.findViewById(R.id.button_save);
+
+        this.setListeners();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_save){
+            this.handleSave();
+        }
+    }
+
+    private void setListeners(){
+        this.mViewHolder.mButtonSave.setOnClickListener(this);
+    }
+
+    private void handleSave() {
+        GuestEntity guestEntity = new GuestEntity();
+        guestEntity.setName(this.mViewHolder.mEditName.getText().toString());
+
+        if(this.mViewHolder.mRadioNotConfirmed.isChecked()){
+            guestEntity.setConfirmed(GuestConstants.CONFIRMATION.NOT_CONFIRMED);
+        } else if(this.mViewHolder.mRadioPresent.isChecked()){
+            guestEntity.setConfirmed(GuestConstants.CONFIRMATION.PRESENT);
+        } else{
+            guestEntity.setConfirmed(GuestConstants.CONFIRMATION.ABSENT);
+        }
+    }
+
+    private static class ViewHolder{
+        EditText mEditName;
+        RadioButton mRadioNotConfirmed;
+        RadioButton mRadioPresent;
+        RadioButton mRadioAbsent;
+        Button mButtonSave;
     }
 }
